22a01c1a563687f68563c0204b0de0c3d23e785b
ImIveooo <jb.demaret1@gmail.com>
2020-08-02 13:43:50 +0200

    Ajout sons.

---
+++ letsplays2geopolitical.php
@@ -0,0 +1,55 @@
+<?php
+    include ('header.php'); 
+?>
+    <script>
+        function PlaySound(son) {
+        var path = "sons/jdg/s2geopolitical/ep1/"
+        var snd = new Audio(path + son + ".ogg");
+        snd.play();
+        }
+        function StopSound() {
+        snd.stop();
+        }
+    </script>
+<h1 style="color: whitesmoke; text-align: center; font-family: Space Mono, monospace;">Soundboard-ImIveooo_</h1>
+    <section>
+        <details open>
+            <summary>Joueur du Grenier <i class="fa fa-gamepad"></i></summary>
+            <div>
+              <img src="/soundboard/images/jdg.png" alt="jdg"/>
+              <p>
+                Joueur du Grenier est également le nom du personnage principal de l'émission, interprété par le vidéaste Frédéric Molas (dit Fred), et celui de la chaîne YouTube qu'il gère conjointement avec Sébastien Rassiat (dit Seb), cocréateur de l'émission.
+              </p>
+              <p style="text-align: center;">
+                <a href="https://www.youtube.com/user/joueurdugrenier" target="blank"><button class="buttonReseau">Youtube <i class="fa fa-youtube-play"></i></button></a>
+                <a href="http://www.joueurdugrenier.fr/" target="blank"><button class="buttonReseau">Site <i class="fa fa-internet-explorer"></i></button></a>
+                <a href="https://twitter.com/Frederic_Molas" target="blank"><button class="buttonReseau">Twitter <i class="fa fa-twitter"></i></button></a>
+              </p>
+            </div>
+          <p class="sounds">
+            (Let's play Narratif) - NOTRE PROJET- Episode 1 : <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - cest fiable')" class="button"><i class="fa fa-play"></i> C'est fiable ?</button> <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - cest incroyable')" class="button"><i class="fa fa-play"></i> C'est incroyable !</button>
+            <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - cest moi le peuple')" class="button"><i class="fa fa-play"></i> C'est moi le peuple.</button> <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - clio')" class="button"><i class="fa fa-play"></i> C'est le prix d'une clio.</button>
+            <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - de rien')" class="button"><i class="fa fa-play"></i> De rien.</button> <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - de toute facon')" class="button"><i class="fa fa-play"></i> De toute façon.</button>
+            <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - encore')" class="button"><i class="fa fa-play"></i> Encore ce faire...</button> <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - excuser moi')" class="button"><i class="fa fa-play"></i> Excuser moi.</button>
+            <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - je suis contre')" class="button"><i class="fa fa-play"></i> JE SUIS CONTRE !</button> <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - la neutralite')" class="button"><i class="fa fa-play"></i> La neutralité de votre information.</button>
+            <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - la province')" class="button"><i class="fa fa-play"></i> La province.</button> <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - la quoi')" class="button"><i class="fa fa-play"></i> La quoi ?</button>
+            <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - le camembert')" class="button"><i class="fa fa-play"></i> Le camembert.</button> <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - les voitures')" class="button"><i class="fa fa-play"></i> Les voitures de nos jours.</button>
+            <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - les wc')" class="button"><i class="fa fa-play"></i> Les toilettes.</button> <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - mais cest')" class="button"><i class="fa fa-play"></i> Mais c'est des...</button>
+            <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - moule')" class="button"><i class="fa fa-play"></i> Une moule sur une bateau.</button> <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - nous tous')" class="button"><i class="fa fa-play"></i>Nous tous.</button>
+            <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - on envoie pas la police')" class="button"><i class="fa fa-play"></i> On envoie pas la police.</button> <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - que diable')" class="button"><i class="fa fa-play"></i> Que diable !</button>
+            <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - rire')" class="button"><i class="fa fa-play"></i> <i>Rire</i>.</button> <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - seulement pour')" class="button"><i class="fa fa-play"></i> Seulement pour...</button>
+            <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - t con')" class="button"><i class="fa fa-play"></i> Tes c**.</button> <button onclick="PlaySound('jdg -  letsplay s2 GeoPolitical - un reflexe')" class="button"><i class="fa fa-play"></i> Une reflexe.</button>
+        </details>
+        <br>
+        <p style="text-align: center; font-family: Space Mono, monospace; font-size: 16px;">La vidéo.</p>
+        <div class="embedresize">
+            <div>
+                <p style="text-align: center;">
+                    <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/oSqQmsECuy8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </p>
+            </div>
+        </div>
+          <!-- ET NAN C'EST PAS UN BONTON STOP x)-->
+          <p style="text-align: center;"><a href="https://youtu.be/dQw4w9WgXcQ"><button class="buttonStop">Stop <i class="fa fa-stop"></i></button></a></p>
+      </section>
+<?php include ('footer.php'); ?>
